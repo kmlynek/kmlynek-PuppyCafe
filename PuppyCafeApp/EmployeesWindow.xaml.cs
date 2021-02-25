e9e93cd1df02159dce3d383b2a760def96368eeb
kmlynek <kmlynek65@gmail.com>
2021-02-25 13:11:35 +0100

    done employees buttons

--- PuppyCafeApp/EmployeesWindow.xaml.cs
+++ PuppyCafeApp/EmployeesWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,20 +20,69 @@ namespace PuppyCafeApp
     /// </summary>
     public partial class EmployeesWindow : Window
     {
+
+        DatabaseEntities employeesContext = new DatabaseEntities();
+        CollectionViewSource employeesView;
+
+
         public EmployeesWindow()
         {
             InitializeComponent();
+            employeesView = ((CollectionViewSource)(FindResource("employeesViewSource")));
+            DataContext = this;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            employeesContext.employees.Load();
+            employeesView.Source = employeesContext.employees.Local;
+        }
 
-            PuppyCafeApp.DatabaseDataSet databaseDataSet = ((PuppyCafeApp.DatabaseDataSet)(this.FindResource("databaseDataSet")));
-            // Load data into the table employees. You can modify this code as needed.
-            PuppyCafeApp.DatabaseDataSetTableAdapters.employeesTableAdapter databaseDataSetemployeesTableAdapter = new PuppyCafeApp.DatabaseDataSetTableAdapters.employeesTableAdapter();
-            databaseDataSetemployeesTableAdapter.Fill(databaseDataSet.employees);
-            System.Windows.Data.CollectionViewSource employeesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("employeesViewSource")));
-            employeesViewSource.View.MoveCurrentToFirst();
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                employees createEmployee = new employees();
+
+                createEmployee.name = nameTextBox.Text;
+                createEmployee.surname = surnameTextBox.Text;
+                createEmployee.pesel = peselTextBox.Text;
+
+                employeesContext.employees.Add(createEmployee);
+                employeesContext.SaveChanges();
+                employeesView.View.Refresh();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Check data or select item", "Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var selectedItem = employeesView.View.CurrentItem as employees;
+
+                var selectedItemDelete = (from c in employeesContext.employees
+                                          where c.employee_id == selectedItem.employee_id
+                                          select c).FirstOrDefault();
+
+                employeesContext.employees.Remove(selectedItemDelete);
+                employeesContext.SaveChanges();
+                employeesView.View.Refresh();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Check data or select item", "Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            employeesView.View.Refresh();
         }
     }
 }
